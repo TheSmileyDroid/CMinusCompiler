98c3c73bd2df8cd7c6924e240da76fa68061d8f2
TheSmileyDroid <ghsilha@gmail.com>
2024-12-01 23:41:42 -0300

    refactor: ajustar caminhos de arquivos de teste e remover comentários desnecessários

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
-// src/main.cpp
-
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    // Initialize compiler components
-
     std::cout << "C- Compiler Initialized." << std::endl;
 
-    // TODO: Integrate lexical analyzer, parser, semantic analyzer, etc.
-
-    // Example usage:
-    // Lexer lexer;
-    // Parser parser;
-
-    // Process input file or code here
+    
 
     return 0;
 }
