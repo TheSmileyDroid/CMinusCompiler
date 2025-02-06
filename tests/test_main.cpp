5b6126eb489f1b3dc51dd9ead091a812fbb6080b
Gabriel H. Silva <ghsilha@gmail.com>
2025-02-05 22:46:46 -0300

    feat: adicionar teste de erro para o parser no Google Test

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -15,6 +15,12 @@ TEST(ParserTest, BasicAssertions) {
   ASSERT_EQ(0, result);
 }
 
+TEST(ParserTest, ErrorAssertions) {
+  std::string command = "compiler ./tests/gcd.1.txt";
+  int result = system(command.c_str());
+  ASSERT_EQ(1, result);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
